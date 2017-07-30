858c1b42086d8d2366dd594e566cb02f28377a91
marcinfoss <marcin.foss@gmail.com>
2017-07-30 21:17:02 +0200

    registration
    added hash to mail and password

--- addUser.php
+++ addUser.php
@@ -13,17 +13,25 @@ if ($conn->connect_error) {
 mysqli_set_charset($conn,"utf8");
 
 $name = $_POST['name'];
-$mail = $_POST['mail'];
-$passwd = test_input($_POST['passwd']);
+$mail = crypt($_POST['mail']);
+$passwd = crypt(test_input($_POST['passwd']));
 
-$query= "INSERT INTO quizers (login, mail, passwd)
+$query= "INSERT INTO quizers (login, mail, password)
 VALUES ('$name',
 '$mail',
 '$passwd'
     );";
 
+
 if( $result = $conn->query($query)) { echo "Dodano użytkownika!"; }
 else echo "Pojawił się błąd!";
 
 mysqli_close($conn);
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
